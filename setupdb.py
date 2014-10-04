b971b09c226f300dc43ed6ff1f3a2c5beaa25a62
jonsag <jonsagebrand@gmail.com>
2014-10-05 00:15:58 +0200

    Changed ISP column in db

--- setupdb.py
+++ setupdb.py
@@ -46,7 +46,7 @@ def columnsConfig(verbose):
             ["region", "varchar(20)"],
             ["country", "varchar(20)"],
             ["regionCode", "varchar(3)"],
-            ["isp", "varchar(30)"],
+            ["isp", "varchar(40)"],
             ["geoSource", "varchar(30)"]]
     
     for column, value in set1:
