d2b006d9d8009c2344ca7950e419f5a31cd8b390
jonsag <jonsagebrand@gmail.com>
2014-10-05 18:56:32 +0200

    Changed column timestamp from timestamp to datetime

--- setupdb.py
+++ setupdb.py
@@ -33,7 +33,7 @@ def columnsConfig(verbose):
         print "--- Reading columns config..."
     
     table1 = tableName   
-    set1 = [["timeStamp", "timestamp"],
+    set1 = [["timeStamp", "datetime"],
             ["name", "varchar(10)"],
             ["protocol", "varchar(3)"],
             ["port", "int(5)"],
